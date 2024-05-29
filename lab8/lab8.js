ae8f63f25b5b0d350a0a5fb4678b498735566a63
Екатерина Кузьменко <katya.kuzmen@yandex.ru>
2024-05-29 15:30:41 +0700

    День недели

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -14,4 +14,16 @@ function showDate() {
     out.innerHTML += '<br>Текущий месяц: ' + (today.getMonth() + 1);
     out.innerHTML += '<br>Текущая дата: ' + today.getDate();
     out.innerHTML += '<br>День недели: ' + today.toLocaleString('ru-RU', { weekday: 'long' });
+
+    document.getElementById('submit-button').addEventListener('click', function() {
+        let day = parseInt(document.getElementById('day').value);
+        let month = parseInt(document.getElementById('month').value) - 1;
+        let year = parseInt(document.getElementById('year').value);
+    
+        let date = new Date(year, month, day);
+        let result = document.getElementById('result');
+    
+        result.innerHTML = 'День недели: ' + date.toLocaleString('ru-RU', { weekday: 'long' });
+      });
 }
+
