72bc43f9c35f9347d11a9def63bc45815160d2bd
Екатерина Кузьменко <katya.kuzmen@yandex.ru>
2024-05-29 16:00:34 +0700

    Количество дней с даты рождения

---
+++ lab8/get-days-count.js
@@ -0,0 +1,11 @@
+function showDaysCount() {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+    document.getElementById('result').innerHTML = `Количество дней с даты рождения: ${daysCount}`;
+    document.querySelector('button[type=reset]').addEventListener('click', function() {
+        document.querySelector('input[type=date]').value = '';
+        document.getElementById('result').innerHTML = '';});
+}
